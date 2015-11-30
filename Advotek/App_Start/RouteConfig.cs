638cd8bcbd08c5b657915a34d0470c0496fb9941
tosca ragnini <tosca.ragnini@gmail.com>
2015-11-30 02:43:21 -0600

    added external HP page

--- Advotek/App_Start/RouteConfig.cs
+++ Advotek/App_Start/RouteConfig.cs
@@ -12,7 +12,42 @@ namespace Advotek
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+                "Index",
+                "Index",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                "about",
+                "About",
+                defaults: new { controller = "Home", action = "Index#about", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                "services",
+                "Services",
+                defaults: new { controller = "Home", action = "Index#services", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                "clients",
+                "Clients",
+                defaults: new { controller = "Home", action = "Index#clients", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+                "contact",
+                "Contact",
+                defaults: new { controller = "Home", action = "Index#contact", id = UrlParameter.Optional }
+            );
+            routes.MapRoute(
+               name: "HPprinters",
+               url: "HPprinters",
+               defaults: new { controller = "Home", action = "HPprinters", id = UrlParameter.Optional }
+           );
 
+            routes.MapRoute(
+              name: "HPproducts",
+              url: "Hpproducts",
+              defaults: new { controller = "Home", action = "HPproducts", id = UrlParameter.Optional }
+          );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Advotek/Controllers/HomeController.cs
+++ Advotek/Controllers/HomeController.cs
@@ -10,22 +10,21 @@ namespace Advotek.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult HPproducts()
         {
-            ViewBag.Message = "Your app description page.";
+            return View();
+        }
 
+        public ActionResult HPprinters()
+        {
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
