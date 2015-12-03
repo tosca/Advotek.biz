8a0512372cf43ea495aabb4774bf2c989d26bfde
tosca ragnini <tosca.ragnini@gmail.com>
2015-12-03 01:37:46 -0600

    added all one page hp to nav

--- Advotek/App_Start/RouteConfig.cs
+++ Advotek/App_Start/RouteConfig.cs
@@ -62,6 +62,98 @@ namespace Advotek
          );
 
             routes.MapRoute(
+            name: "HPbusinessUpgradeTool",
+            url: "HPbusinessUpgradeTool",
+            defaults: new { controller = "Home", action = "HPbusinessUpgradeTool", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPeliteBookFamily",
+            url: "HPeliteBookFamily",
+            defaults: new { controller = "Home", action = "HPeliteBookFamily", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPenterpriseGroup",
+            url: "HPenterpriseGroup",
+            defaults: new { controller = "Home", action = "HPenterpriseGroup", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPhelpdesk",
+            url: "HPhelpdesk",
+            defaults: new { controller = "Home", action = "HPhelpdesk", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPimagePrintPromo",
+            url: "HPimagePrintPromo",
+            defaults: new { controller = "Home", action = "HPimagePrintPromo", id = UrlParameter.Optional }
+        );
+
+
+            routes.MapRoute(
+            name: "HPK12Education",
+            url: "HPK12Education",
+            defaults: new { controller = "Home", action = "HPK12Education", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPmobility",
+            url: "HPmobility",
+            defaults: new { controller = "Home", action = "HPmobility", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPofficeProFamily",
+            url: "HPofficeProFamily",
+            defaults: new { controller = "Home", action = "HPofficeProFamily", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPPCupgradetool",
+            url: "HPPCupgradetool",
+            defaults: new { controller = "Home", action = "HPPCupgradetool", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPPrintImageCarePack",
+            url: "HPPrintImageCarePack",
+            defaults: new { controller = "Home", action = "HPPrintImageCarePack", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPproFamily",
+            url: "HPproFamily",
+            defaults: new { controller = "Home", action = "HPproFamily", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPscanJetScanners",
+            url: "HPscanJetScanners",
+            defaults: new { controller = "Home", action = "HPscanJetScanners", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPwindows10",
+            url: "HPwindows10",
+            defaults: new { controller = "Home", action = "HPwindows10", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "HPworkstationFamily",
+            url: "HPworkstationFamily",
+            defaults: new { controller = "Home", action = "HPworkstationFamily", id = UrlParameter.Optional }
+        );
+
+            routes.MapRoute(
+            name: "originalHPsupplies",
+            url: "originalHPsupplies",
+            defaults: new { controller = "Home", action = "originalHPsupplies", id = UrlParameter.Optional }
+        );
+
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- Advotek/Controllers/HomeController.cs
+++ Advotek/Controllers/HomeController.cs
@@ -37,5 +37,67 @@ namespace Advotek.Controllers
         {
             return View();
         }
+
+        public ActionResult HPbusinessUpgradeTool()
+        {
+            return View();
+        }
+        public ActionResult HPeliteBookFamily()
+        {
+            return View();
+        }
+        public ActionResult HPenterpriseGroup()
+        {
+            return View();
+        }
+        public ActionResult HPhelpdesk()
+        {
+            return View();
+        }
+        public ActionResult HPimagePrintPromo()
+        {
+            return View();
+        }
+        public ActionResult HPK12Education()
+        {
+            return View();
+        }
+        public ActionResult HPmobility()
+        {
+            return View();
+        }
+        public ActionResult HPofficeProFamily()
+        {
+            return View();
+        }
+        public ActionResult HPPCupgradetool()
+        {
+            return View();
+        }
+        public ActionResult HPPrintImageCarePack()
+        {
+            return View();
+        }
+        public ActionResult HPproFamily()
+        {
+            return View();
+        }
+        public ActionResult HPscanJetScanners()
+        {
+            return View();
+        }       
+
+        public ActionResult HPwindows10()
+        {
+            return View();
+        }
+        public ActionResult HPworkstationFamily()
+        {
+            return View();
+        }
+        public ActionResult originalHPsupplies()
+        {
+            return View();
+        }
     }
 }
