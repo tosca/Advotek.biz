8105e25eb5f90468cff3e51d8676523814370201
tosca ragnini <tosca.ragnini@gmail.com>
2016-01-10 21:03:57 -0600

    added mailgun and contact form

--- Advotek/Controllers/HomeController.cs
+++ Advotek/Controllers/HomeController.cs
@@ -33,10 +33,10 @@ namespace Advotek.Controllers
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            return View();
-        }
+       // public ActionResult Contact()
+       // {
+       //     return View();
+       // }
 
         public ActionResult HPbusinessUpgradeTool()
         {
@@ -99,5 +99,78 @@ namespace Advotek.Controllers
         {
             return View();
         }
+
+
+        [HttpGet]
+        public ActionResult ContactForm()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View(new ContactInfo());
+        }
+        [HttpPost]
+        public ActionResult Contact(ContactInfo contact)
+        {
+
+            var Name = contact.Name;
+            var contactEmail = contact.ContactEmail;
+            var contactPhone = contact.ContactPhone;
+            var contactMessage = contact.ContactMessage;
+            SendEmail(Name, contactEmail, contactPhone, contactMessage);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ContactSubmited()
+        {
+
+            var result = new FilePathResult("~/Views/Home/Index.html", "text/html");
+            return result;
+
+        }
+
+
+        public void SendEmail(string Name, string contactEmail, string contactPhone, string contactMessage)
+        {
+            string mailgunKey = System.Configuration.ConfigurationManager.AppSettings["MAILGUN_API_KEY"];
+            if (String.IsNullOrEmpty(mailgunKey))
+            {
+                throw new Exception("Mailgun api key missing");
+            }
+            RestClient client = new RestClient();
+            client.BaseUrl = new Uri("https://api.mailgun.net/v3");
+            client.Authenticator =
+                    new HttpBasicAuthenticator("api",
+                                               mailgunKey);
+
+            RestRequest request = new RestRequest();
+            request.AddParameter("domain",
+                                 System.Configuration.ConfigurationManager.AppSettings["MAILGUN_DOMAIN"],
+                                 ParameterType.UrlSegment);
+            request.Resource = "{domain}/messages";
+            request.AddParameter("from", "Paula - inquiry <mailgun@mailgun.org>");
+            request.AddParameter("to", "tosca.ragnini@gmail.com, paula@advotek.biz");
+            request.AddParameter("subject", "Advotek - Contact Request");
+            request.AddParameter("text", "Name : " + Name);
+            request.AddParameter("text", "ContactEmail : " + contactEmail);
+            request.AddParameter("text", "ContactPhone : " + contactPhone);
+            request.AddParameter("text", "ContactMessage : " + contactMessage);
+            request.Method = Method.POST;
+            var result = client.Execute(request);
+            return;
+        }
+
+
     }
+
+
+    public class ContactInfo
+    {
+        public string Name { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactPhone { get; set; }
+        public string ContactMessage { get; set; }
+
+    }
+
 }
+
